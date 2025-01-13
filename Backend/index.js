346232797d0ce2f01e0b9e59bc6588e88d4c9c67
Enzoputachi <samuelnzoputachi@gmail.com>
2025-01-13 11:21:36 +0100

    create application entry point

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+import { connectDB } from "./config/db.js";
+
+
+
+dotenv.config();
+const port = process.env.PORT || 4200;
+
+// Connect DB
+connectDB()
+
+const app = express();
+
+// mount middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Listen for request
+app.listen(port, () => console.log(`Server runing on port: ${port}`))
