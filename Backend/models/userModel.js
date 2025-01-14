44c5d4c4f5f73fc204f54a1ef95840332f3e58b2
Enzoputachi <samuelnzoputachi@gmail.com>
2025-01-14 08:43:13 +0100

    create user model

---
+++ Backend/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        fullname : {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamp: true
+    }
+)
+
+const userModel = mongoose.model('User', userSchema);
+export default userModel;
