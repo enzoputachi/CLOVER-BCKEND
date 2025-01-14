99684909aaba4e07f3177f9a8cae24c7bd46cb1e
Enzoputachi <samuelnzoputachi@gmail.com>
2025-01-14 08:42:23 +0100

    Add regiser andlogin services

---
+++ Backend/services/userAuthServices.js
@@ -0,0 +1,51 @@
+import userModel from "../models/userModel.js"
+import { comparePassword, hashPassword } from "../utils/hashUtils";
+import { generateToken } from "../utils/tokenUtils.js";
+
+
+export const registerService = async(fullname, email, password) => {
+    try {
+        const userExists = await userModel.findOne({ email });
+        if (userExists) throw new Error('User already exists');
+
+        const hashedPassword = await hashPassword(password);
+        const newUser = new User({ email, password: hashedPassword, fullname});
+
+        await newUser.save()
+
+        const token = generateToken(newUser._id);
+
+        return {
+            _id: newUser._id,
+            fullname: newUser.fullname,
+            email: newUser.email,
+            token,
+        };
+    } catch (error) {
+        console.error('Error in register:', error.message);
+        throw new Error(error.message)
+    }
+};
+
+
+export const loginService = async(email, password) => {
+    try {
+        const existingUser = await userModel.findOne({ email });
+        if (!existingUser) throw new Error('Invalid email or password');
+
+        const isPasswordValid = await comparePassword(password, existingUser.password);
+        if (!isPasswordValid) throw new Error('Invalid email or password');
+
+        const token = generateToken(existingUser._id);
+
+        return {
+            _id: existingUser._id,
+            fullname: existingUser.fullname,
+            email: existingUser.email,
+            token,
+        };
+    } catch (error) {
+        console.error('Error during sigin-in:', error.message);
+        throw new Error(error.message || 'Internal server error');
+    }
+}
