0aedd36921afde9b203b2b7fcc71c7fcd665f7be
Enzoputachi <samuelnzoputachi@gmail.com>
2025-01-24 09:21:01 +0100

    Define course model

---
+++ Backend/models/courseModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const courseSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+        },
+        videos: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Video',
+        },
+        outline: [{
+            type: String,
+        }],
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Category',
+        },
+    },
+    {
+        timestamp: true,
+    }
+)
+
+export const courseModel = mongoose.model('Course', courseSchema)
