8306c6e924bbb0fddd691cb6f7b2b8a7e9f96139
Enzoputachi <samuelnzoputachi@gmail.com>
2025-01-27 07:54:36 +0100

    Update code

--- Backend/routes/userAuthRoutes.js
+++ Backend/routes/userAuthRoutes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import { getCurrentUserProfile, login, logout, register } from '../controllers/userAuthController.js';
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
+import { updateCurrentUserProfile } from '../services/userAuthServices.js';
 
 const router = express.Router();
 
-router.route('/')
-    .post(register)
-
+router.post('/', register)
 router.post('/login', login)
 router.post('/logout', logout)
 router.get('/profile', authenticate, getCurrentUserProfile)
+router.patch('/profile', authenticate, updateCurrentUserProfile)
 
 
 export default router;
